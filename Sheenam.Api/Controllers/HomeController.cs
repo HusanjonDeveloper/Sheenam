bd859b887ffa729937907b51e366690d4a9b553b
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-11 19:34:04 +0500

    INFRA: Initialize Api Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿//======================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free ToUse Comfort and Peace 
+//======================
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers; 
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/Controller")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult <string> Get() =>
+            Ok("Hello marion, the princess is in another castle:");
+    }
+}

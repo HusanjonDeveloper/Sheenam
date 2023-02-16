11204474f9357e634961f7a49269276e6c06c769
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-16 07:47:19 +0500

    DATA: Model  & Migrate Guests

---
+++ Sheenam.Api/Models/Foundations/Guests/Guest.cs
@@ -0,0 +1,23 @@
+﻿//======================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free ToUse Comfort and Peace 
+//======================
+
+
+using System;
+
+namespace Sheenam.Api.Models.Foundations.Guests
+{
+    public class Guest
+    {
+        public Guid Id { get; set; }
+        public string  FirstName{ get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBieth { get; set; }
+        public string Emile { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Adderss { get; set; }
+        public GenderType Gender { get; set; }
+
+    }
+}

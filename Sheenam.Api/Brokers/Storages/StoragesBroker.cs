3325388783db7d2df85ea1ef9d4e1d2eb2458e9f
Muhammadaliyev Husanjon <120047912+HusanjonDeveloper@users.noreply.github.com>
2023-02-20 17:46:43 +0500

    Revert "Revert "DATA: Model  & Migrate Guests""

---
+++ Sheenam.Api/Brokers/Storages/StoragesBroker.cs
@@ -0,0 +1,33 @@
+﻿//======================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free ToUse Comfort and Peace 
+//======================
+
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial class StoragesBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StoragesBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connetionString =
+                this.configuration.GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connetionString);
+        }
+
+        public override void Dispose (){}
+    }
+}
---
+++ Sheenam.Api/Migrations/StoragesBrokerModelSnapshot.cs
@@ -0,0 +1,63 @@
+﻿//======================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free ToUse Comfort and Peace 
+//======================
+
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Sheenam.Api.Brokers.Storages;
+
+#nullable disable
+
+namespace Sheenam.Api.Migrations
+{
+    [DbContext(typeof(StoragesBroker))]
+    partial class StoragesBrokerModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder
+                .HasAnnotation("ProductVersion", "7.0.3")
+                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+
+            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+
+            modelBuilder.Entity("Sheenam.Api.Models.Foundations.Guests.Guest", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<string>("Adderss")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<DateTimeOffset>("DateOfBieth")
+                        .HasColumnType("datetimeoffset");
+
+                    b.Property<string>("Emile")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("FirstName")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<int>("Gender")
+                        .HasColumnType("int");
+
+                    b.Property<string>("LastName")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("PhoneNumber")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Guests");
+                });
+#pragma warning restore 612, 618
+        }
+    }
+}

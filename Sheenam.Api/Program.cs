25020f7495ae12ddf64c92ced305307918c3dd97
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-09-14 22:03:35 +0500

    Code rub

--- Sheenam.Api/Program.cs
+++ Sheenam.Api/Program.cs
@@ -13,6 +13,7 @@ namespace Sheenam.Api
         public static void Main(string[] args) =>
            CreateHostBuilder(args).Build().Run();
 
+
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
